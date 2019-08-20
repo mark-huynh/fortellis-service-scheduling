7a71fcb5edce01554168f651c920cbe625d98843
Mark Huynh <mark.huynh@cdk.com>
2019-08-20 13:11:15 -0700

    Added component library

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import {Spinner} from 'native-base'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,6 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Spinner></Spinner>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
