92e3368e14801adece8f55e60b80b03562c5cfc7
Han, Kelly <Kelly.Han@cdk.com>
2019-08-22 15:45:41 -0700

    ScreenContainer updated with form

--- ScreenContainer.js
+++ ScreenContainer.js
@@ -4,14 +4,16 @@ import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import AddEventScreen from './AddEventScreen';
 import CardInfo from './CardInfo';
-import DetailsScreen from './DetailsScreen'
+import DetailsScreen from './DetailsScreen';
+import form from './form';
 
 const NavigationStack = createStackNavigator({
     LoginScreen: { screen: LoginScreen },
     HomeScreen: { screen: HomeScreen },
     AddEventScreen: { screen: AddEventScreen },
     DetailsScreen: { screen: DetailsScreen },
-    CardInfo: { screen: CardInfo }
+    CardInfo: { screen: CardInfo },
+    form: {screen: form}
 });
 
 const Container = createAppContainer(NavigationStack);
