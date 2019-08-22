16a9ce29dfbaa30a5e7cf13949d58c3b65609b02
Kinch, Thomas <Thomas.Kinch@cdk.com>
2019-08-22 15:19:48 -0700

    Styled Details Page

--- DetailsScreen.js
+++ DetailsScreen.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet, Linking } from 'react-native';
 import { Container, Fab, Icon } from 'native-base';
+
+const styles = StyleSheet.create({
+  title: {
+      backgroundColor: '#5067FF',
+      color: '#FFFFFF',
+      fontWeight: 'bold',
+      fontSize: 20
+  },
+  field: {
+      fontSize: 15
+  },
+});
 
 class DetailsScreen extends React.Component {
     static navigationOptions = {
@@ -12,27 +24,30 @@ class DetailsScreen extends React.Component {
             color: '#FFFFFF',
         },
     };
-
+   
     render() {
         return (
             <View>
-                <Text>Appointment ID: {this.props.navigation.getParam('appointmentID', 999)}</Text>
-                <Text>Appointment Date: {this.props.navigation.getParam('dateTime', 999)}</Text>
-                <Text>Description:</Text>
-                <Text>{this.props.navigation.getParam('description', 999)}</Text>
+                <Text style={styles.title} >Appointment Information</Text>
+                <Text style={styles.field} >Appointment ID: {this.props.navigation.getParam('appointmentID', 999)}</Text>
+                <Text style={styles.field} >Appointment Date: {this.props.navigation.getParam('dateTime', 999)}</Text>
+                <Text style={styles.field} >Description:</Text>
+                <Text style={styles.field} >{this.props.navigation.getParam('description', 999)}</Text>
 
-                <Text>Vehicle Information</Text>
-                <Text>{this.props.navigation.getParam('vehicleUnits', 999)} : {this.props.navigation.getParam('vehicleValue', 999)}</Text>
-                <Text>MORE INFO COULD BE OPTAINED USING VEHICLE API</Text>
+                <Text style={styles.title} >Vehicle Information</Text>
+                <Text style={styles.field} >{this.props.navigation.getParam('vehicleUnits', 999)} : {this.props.navigation.getParam('vehicleValue', 999)}</Text>
+                <Text style={styles.field} >MORE INFO COULD BE OPTAINED USING VEHICLE API</Text>
 
-                <Text>Customer Information</Text>
-                <Text>Customer ID: "NEED CUSTOMER API CALL" </Text>
-                <Text>LastName, FirstName : "NEED CUSTOMER API CALL"</Text>
-                <Text>Email: "NEED CUSTOMER API CALL"</Text>
-                <Text>Phone Number: ({this.props.navigation.getParam('phoneType', 999)}) {this.props.navigation.getParam('phoneNumber', 999)}</Text>
-                <Text>Availability</Text>
-                <Text>{this.props.navigation.getParam('startDay', 999)} - {this.props.navigation.getParam('endDay', 999)}</Text>
-                <Text>From {this.props.navigation.getParam('startTime', 999)} to {this.props.navigation.getParam('endTime', 999)} </Text>
+                <Text style={styles.title} >Customer Information</Text>
+                <Text style={styles.field} >Customer ID: "NEED CUSTOMER API CALL" </Text>
+                <Text style={styles.field} >LastName, FirstName : "NEED CUSTOMER API CALL"</Text>
+                <Text style={styles.field} >Email: "NEED CUSTOMER API CALL"</Text>
+                <Text style={styles.field} onPress={ () => Linking.openURL(this.props.navigation.getParam('phoneNumber', 'tel:111-111-1111'))}>
+                    Phone Number: ({this.props.navigation.getParam('phoneType', 999)}) {this.props.navigation.getParam('phoneNumber', 999)}
+                </Text>
+                <Text style={styles.field} >Availability</Text>
+                <Text style={styles.field} >{this.props.navigation.getParam('startDay', 999)} - {this.props.navigation.getParam('endDay', 999)}</Text>
+                <Text style={styles.field} >From {this.props.navigation.getParam('startTime', 999)} to {this.props.navigation.getParam('endTime', 999)} </Text>
             </View>
         )
     }
